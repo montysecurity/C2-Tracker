3ff56f8a561aa35a3e7445b867447b49fe03ca88
montysecurity <montysecurity@protonmail.com>
2022-12-01 17:48:51 +0000

    formatting

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 tracker.py && git add . && git commit -a -m "Nightly Auto Update" && git push
+python3 tracker.py && echo && echo && git add . && git commit -a -m "Nightly Auto Update" && git push
