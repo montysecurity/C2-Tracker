0990631a2b80b78031b6029eb4ed82a0304a8120
montysecurity <18372298+montysecurity@users.noreply.github.com>
2023-10-19 20:43:47 +0000

    Nightly Auto Update

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git pull && python3 tracker.py && echo && echo && git add . && find -size 0c -delete && git commit -a -m "Nightly Auto Update" && git push
+git pull
+python3 tracker.py && echo && echo && git add . && find -size 0c -delete && git commit -a -m "Nightly Auto Update" && git push
